dd581f170e77abc7965aa41bd637e8a34755d537
WangZhijianZJU <35230981+WangZhijianZJU@users.noreply.github.com>
2022-12-21 21:52:57 +0800

    Update plot_Dyn_Exp_Acc20221220.m

--- plot_Dyn_Exp_Acc20221220.m
+++ plot_Dyn_Exp_Acc20221220.m
@@ -1,38 +1,40 @@
+% >> ofilehead = 'Exp'; plot_Dyn_Exp_Acc20221220(ofilehead);
+% >> ofilehead = 'Dyn'; plot_Dyn_Exp_Acc20221220(ofilehead);
 % C:\Users\think\Documents\MATLAB\plot_Dyn_Exp_Acc20221220.m
-function r = plot_Dyn_Exp_Acc20221220()
-    close all 
-    clear 
-                                      p=[6 8 10]; 
+function r = plot_Dyn_Exp_Acc20221220(ofilehead)
+%     close all 
+%     clear 
+                                      p=[6 8 10];             leg ={'(2,1)', '(3,2)', '(4,2)'}
  %% ----------------------------------------------------------------------------------------------------------------%%   
-% % % % % % % % % % % % % % % % % % % % % % % %     ofilehead = 'exp';
-% % % % % % % % % % % % % % % % % % % % % % % %     load('eicycle.mat')  % experimental result  
-% % % % % % % % % % % % % % % % % % % % % % % %    
-% % % % % % % % % % % % % % % % % % % % % % % %     Acc_3page=[]
-% % % % % % % % % % % % % % % % % % % % % % % %     for k=1:3
-% % % % % % % % % % % % % % % % % % % % % % % % 
-% % % % % % % % % % % % % % % % % % % % % % % %         n8x8list_4 = n8x8list_3(n8x8list_3(:,9)==p(k),:);
-% % % % % % % % % % % % % % % % % % % % % % % % 
-% % % % % % % % % % % % % % % % % % % % % % % %         n8x8list_4=sortrows(n8x8list_4, [3 9 1 5 4]);
-% % % % % % % % % % % % % % % % % % % % % % % %         r0=[];
-% % % % % % % % % % % % % % % % % % % % % % % %             for t=1:1000
-% % % % % % % % % % % % % % % % % % % % % % % %                 r1 = n8x8list_4(n8x8list_4(:,3)==t,:);
-% % % % % % % % % % % % % % % % % % % % % % % %                 r0=[r0; sum(r1(:,11:26))];
-% % % % % % % % % % % % % % % % % % % % % % % %                     if t==1; 
-% % % % % % % % % % % % % % % % % % % % % % % %                         Acc_3page(t,:,k) = r0;
-% % % % % % % % % % % % % % % % % % % % % % % %                     else 
-% % % % % % % % % % % % % % % % % % % % % % % %                         Acc_3page(t,:,k) = sum(r0);
-% % % % % % % % % % % % % % % % % % % % % % % %                     end
-% % % % % % % % % % % % % % % % % % % % % % % %             end
+ if ofilehead == 'Exp'     
+    load('eicycle.mat')  % experimental result   
+    Acc_3page=[]
+    for k=1:3 
+        n8x8list_4 = n8x8list_3(n8x8list_3(:,9)==p(k),:); 
+        n8x8list_4=sortrows(n8x8list_4, [3 9 1 5 4]);
+        r0=[];
+            for t=1:1000
+                r1 = n8x8list_4(n8x8list_4(:,3)==t,:);
+                r0=[r0; sum(r1(:,11:26))];
+                    if t==1; 
+                        Acc_3page(t,:,k) = r0;
+                    else 
+                        Acc_3page(t,:,k) = sum(r0);
+                    end
+            end
+     end
  %% ----------------------------------------------------------------------------------------------------------------%% 
-    ofilehead = 'dyn';
+ else if ofilehead == 'Dyn'
     load('J_dyn_202212.mat','R36000x35Logit', 'eicyclelogit','Acc_3page_logit')            
             Acc_3page = Acc_3page_logit;
-    for k=1:3
+     end
+ end    
  %% ----------------------------------------------------------------------------------------------------------------%% 
-         figure; 
+    for k=1:3
+          figure; 
 %          subplot(1,3,1); plot(Acc_3page(:,:,k)); 
 %          subplot(1,2,1); 
-         plot(Acc_3page(:,1:8,k),'linewidth',4); xlim([0,200]);ylim([0 1000]);axis square; %title(strcat('dyn-X',num2str(p(k))))
+         plot(Acc_3page(:,1:8,k),'linewidth',4); xlim([0,400]);ylim([0 1500]);axis square; text(20,980,strcat(ofilehead,'-',leg(k),'-X'),'fontsize',16)
 %            legend('X_1', 'X_2', 'X_3', 'X_4', 'X_5', 'X_6', 'X_7', 'X_8','location','eastoutside') 
            legend('1', '2', '3', '4', '5', '6', '7', '8','location','northwest','numcolumns',2) 
               set(gcf,'papersize',[12 12],'paperposition',[0,0,12,12]);hold on; 
@@ -40,7 +42,7 @@ function r = plot_Dyn_Exp_Acc20221220()
            saveas(gcf,strcat(ofilehead,'__accX200_', num2str(k), '.png'))
          figure; 
 %          subplot(1,2,2); 
-         plot(Acc_3page(:,9:16,k),'linewidth',4);xlim([0,200]);ylim([0 1000]);axis square; %title(strcat('dyn-Y',num2str(p(k))))
+         plot(Acc_3page(:,9:16,k),'linewidth',4);xlim([0,400]);ylim([0 1500]);axis square; text(20,980,strcat(ofilehead,'-',leg(k),'-Y'),'fontsize',16)
 %            legend('Y_1', 'Y_2', 'Y_3', 'Y_4', 'Y_5', 'Y_6', 'Y_7', 'Y_8','location','northwest','numcolumns',2) 
            legend('1', '2', '3', '4', '5', '6', '7', '8','location','northwest','numcolumns',2) 
               set(gcf,'papersize',[12 12],'paperposition',[0,0,12,12]);hold on; 
